9262100e789254803d6c4eaa5974ab8065c6d2c9
nahidn228 <nahidn228@gmail.com>
2025-05-16 23:32:27 +0600

    Data Filtering: WHERE Clause, Logical Operators, and Comparison Operators

--- select.sql
+++ select.sql
@@ -42,3 +42,50 @@ SELECT email as "Student Email", * from students;
 SELECT * FROM students ORDER BY first_name ASC;
 -- sorting using age descending order
 SELECT * from students ORDER BY age DESC;
+
+SELECT country from students ORDER BY country ASC;
+
+-- check unique country 
+SELECT DISTINCT country FROM students;
+
+-- check unique blood_group
+SELECT DISTINCT blood_group FROM students;
+
+-- data filtering
+
+-- Select students from the USA
+-- Select students with 'A' grade in Physics
+-- Select students with a specific blood group ('A+')
+-- Select students from the USA or from Australia
+-- Select students from the USA or from Australia and the age is 20
+-- Select students with a grade of 'A' or 'B' in Math or Physics:
+-- Select students older than 20
+
+-- Select students from the USA
+SELECT * FROM students 
+  WHERE country = 'USA';
+
+
+-- Select students with 'A' grade in Physics
+SELECT * FROM students
+WHERE course = 'Physics' AND grade = 'A';
+
+-- Select students with a specific blood group ('A+')
+SELECT * FROM students 
+WHERE blood_group = 'A+';
+-- Select students from the USA or from Australia
+SELECT * FROM students 
+WHERE country = 'USA' OR country= 'Australia';
+
+-- Select students from the USA or from Australia and the age is 20
+SELECT * FROM students 
+WHERE age = 20 AND (country = 'USA' OR country= 'Australia') ;
+
+-- Select students with a grade of 'A' or 'B' in Math or Physics:
+
+SELECT * FROM students 
+  WHERE (grade = 'A' OR grade = 'B') AND (course = 'Math' OR course = 'Physics');
+
+-- Select students older than 20
+SELECT * FROM students
+WHERE age > 20;
