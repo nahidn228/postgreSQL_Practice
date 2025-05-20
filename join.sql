871bb6b053adb08860c5f28588129c4fe0a1686e
nahidn228 <nahidn228@gmail.com>
2025-05-20 10:33:32 +0600

    Understanding Left and Right Joins

--- join.sql
+++ join.sql
@@ -36,6 +36,24 @@ SELECT * from post
 JOIN "user" ON post.user_id = "user".id;
 
 -- join two table and rename table name
-
+-- Inner JOIN
 SELECT username, title FROM post as p 
 JOIN "user" as u ON p.user_id = u.id;
+
+
+-- Inner JOIN
+SELECT * from "user" 
+JOIN post ON post.user_id = "user".id;
+
+
+INSERT INTO post (id,title, user_id) VALUES
+(5,'This is a test title! ☀️', NULL)
+
+
+--Left JOIN
+SELECT * from "user" 
+LEFT JOIN post ON post.user_id = "user".id;
+
+--RIGHT JOIN
+SELECT * from "user" 
+RIGHT JOIN post ON post.user_id = "user".id;
